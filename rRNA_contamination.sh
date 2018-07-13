f729acc995d07fe647854e3b114d3e7efd3cf8a2
jenni-westoby <jenniwestoby@gmail.com>
2018-07-13 15:03:24 +0100

    fix read polyester issue

---
+++ rRNA_contamination.sh
@@ -0,0 +1,15 @@
+filename=$1
+path_to_java=$2
+
+./Simulation/STAR/bin/Linux_x86_64/STAR --runThreadN 8 --genomeDir Simulation/indices/STAR --readFilesIn ES_cell_data/$filename"_1.fastq" ES_cell_data/$filename"_2.fastq" --outFileNamePrefix ES_cell_data/$filename --outSAMtype BAM SortedByCoordinate
+
+#Use bam_stat from the RSeQC package to find alignment statistics
+source Simulation/venv/bin/activate
+split_bam.py -i ES_cell_data/$filename"Aligned.sortedByCoord.out.bam" -r no_chr_mm10_rRNA.bed -o ES_cell_data/$filename
+deactivate
+rm ES_cell_data/$filename"_1.fastq"
+rm ES_cell_data/$filename"_2.fastq"
+$path_to_java -XX:MaxHeapSize=1000m -jar picard.jar SamToFastq \
+I=ES_cell_data/$filename"Aligned.sortedByCoord.out.bam" \
+FASTQ=ES_cell_data/$filename"_1.fastq" \
+SECOND_END_FASTQ=ES_cell_data/$filename"_2.fastq"
--- second_half_polyester.sh
+++ second_half_polyester.sh
@@ -3,16 +3,21 @@
 filename=`echo $1 |awk -F_ '{print $1}'`
 
 #There is a bug in Polyester which produces 2 reads with the same name for each million and first read. This upsets RSEM so I rename the second of each of these reads here:
-sed -i 's/read1000001/read1000001a/2' Simulation/data/simulated/$1
-sed -i 's/read1000001/read1000001a/2' Simulation/data/simulated/$filename'_2.fq'
-sed -i 's/read2000001/read2000001a/2' Simulation/data/simulated/$1
-sed -i 's/read2000001/read2000001a/2' Simulation/data/simulated/$filename'_2.fq'
-sed -i 's/read3000001/read3000001a/2' Simulation/data/simulated/$1
-sed -i 's/read3000001/read3000001a/2' Simulation/data/simulated/$filename'_2.fq'
-sed -i 's/read4000001/read4000001/2' Simulation/data/simulated/$1
-sed -i 's/read4000001/read4000001a/2' Simulation/data/simulated/$filename'_2.fq'
-sed -i 's/read5000001/read5000001a/2' Simulation/data/simulated/$1
-sed -i 's/read5000001/read5000001a/2' Simulation/data/simulated/$filename'_2.fq'
+sed -i.bak '0,/read1000001/! s/read1000001/read1000001a/' Simulation/data/simulated/$1
+sed -i.bak '0,/read2000001/! s/read2000001/read2000001a/' Simulation/data/simulated/$1
+sed -i.bak '0,/read3000001/! s/read3000001/read3000001a/' Simulation/data/simulated/$1
+sed -i.bak '0,/read4000001/! s/read4000001/read4000001a/' Simulation/data/simulated/$1
+sed -i.bak '0,/read5000001/! s/read5000001/read5000001a/' Simulation/data/simulated/$1
+sed -i.bak '0,/read6000001/! s/read6000001/read6000001a/' Simulation/data/simulated/$1
+
+sed -i.bak '0,/read1000001/! s/read1000001/read1000001a/' Simulation/data/simulated/$filename'_2.fq'
+sed -i.bak '0,/read2000001/! s/read2000001/read2000001a/' Simulation/data/simulated/$filename'_2.fq'
+sed -i.bak '0,/read3000001/! s/read3000001/read3000001a/' Simulation/data/simulated/$filename'_2.fq'
+sed -i.bak '0,/read4000001/! s/read4000001/read4000001a/' Simulation/data/simulated/$filename'_2.fq'
+sed -i.bak '0,/read5000001/! s/read5000001/read5000001a/' Simulation/data/simulated/$filename'_2.fq'
+sed -i.bak '0,/read6000001/! s/read6000001/read6000001a/' Simulation/data/simulated/$filename'_2.fq'
+
+
 # ./quality_control.sh qualitycontrol $filename Simulation/data/simulated '_1.fq' "simulated"
 # ./quantify.sh Kallisto $filename
 # ./quantify.sh eXpress $filename
