19cbed0f0dc17eba1cb91728a43a985bbfa5ca1c
jenni-westoby <jenniwestoby@gmail.com>
2018-07-16 11:59:07 +0100

    fix quantify_real bug

--- cell_level_analysis.sh
+++ cell_level_analysis.sh
@@ -3,6 +3,8 @@
 
 filename=`echo $1 |awk -F_ '{print $1}'`
 
+./quality_control.sh qualitycontrol $filename ES_cell_data '_1.fastq' 'raw'
+./simulate.sh simulate $filename'_1.fastq' ES_cell_data
 ./quality_control.sh qualitycontrol $filename Simulation/data/simulated '_1.fq' "simulated"
 ./quantify.sh Kallisto $filename
 ./quantify.sh eXpress $filename
