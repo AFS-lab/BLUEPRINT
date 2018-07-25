5b833da571e254707160004c0889df6f5a9b5b84
jenni-westoby <jenniwestoby@gmail.com>
2018-07-25 13:46:28 +0100

    remove lsf job commands

--- control_polyester_script.sh
+++ control_polyester_script.sh
@@ -10,5 +10,5 @@ END=`head -n1 raw_results/data/simulated_counts.txt | wc -w`
 #Run polyester simulations, one for each splatter simulated cell
 for i in $(seq 1 $END);
 do
-  bsub -R"span[hosts=1]" -c 99999 -G team_hemberg -q normal -o $TEAM/temp.logs/output.polyester$i -e $TEAM/temp.logs/error.polyester$i -R"select[mem>100000] rusage[mem=100000]" -M 100000 /software/R-3.4.2/bin/Rscript make_polyester.R $i
+   /software/R-3.4.2/bin/Rscript make_polyester.R $i
 done
